bf4511d4d17825216aaf43c8f8896d54bd35ef76
SantiagoMaresca <santiago.maresca26@gmail.com>
2019-06-29 19:27:18 -0300

    cambios en flask1 y form de login

--- flask1.py
+++ flask1.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route('/')
 def home():
-    return render_template('Layout.html')
+    return render_template('Layouaaaat.html')
 
 @app.route('/inicio')
 def inicio():
