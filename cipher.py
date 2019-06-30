ef3fa1d5914e233efefe368c5f5639461f6e734d
Gonzalo Barrios <gonbarrios1@hotmail.com>
2019-06-30 14:48:47 -0300

    cipher final

--- cipher.py
+++ cipher.py
@@ -52,52 +52,54 @@ class Encryptor:
 
 def encriptarArchivo(usuario,pathfrase,ruta):
     try:
-        result=False
-        #Guardar pathfrase en el hash
         key = hashlib.sha256(pathfrase.encode('utf-8')).digest()
-        print(key)
-        #key = str(key).replace(',', '.')
-        #print(key)
         encriptador = Encryptor(key)
         encriptador.encrypt_file(ruta)
-        rutaUsuarios = "/Users/Usuario/Documents/Flask/ArchivosParaEncriptar/usuarioarchivo.txt"
+        rutaUsuarios = "/Users/Gonzalo/Desktop/FACULTAD/2019 s1/Seguridad/ProyectoSeguridad/Proyecto/usuarioarchivo.txt"
         with open(rutaUsuarios, 'a') as file:
             linea = str(usuario) + "," + str(key) + "," + str(ruta + ".enc")
             file.write(linea)
-            result= True
-        return result
-  
-    except :
+            file.write("\n")
+        file.close()
+    except:
         return False
+    return True
 
 def desencriptarArchivo(usuario, pathfrase, ruta):
-    #Checkear si la key es la que se encuentra hasheada
-    result = False
+    res = False
     key = hashlib.sha256(pathfrase.encode('utf-8')).digest()
-    rutaUsuarios = "/Users/Usuario/Documents/Flask/ArchivosParaEncriptar/usuarioarchivo.txt"
-    with open(rutaUsuarios, 'r') as file:
-        lines = file.readlines()
-    with open(rutaUsuarios, 'r') as file:
-        for usuarioArchivo in lines:
-            usuarioArchivoArr = usuarioArchivo.split(",")
-            print(usuarioArchivoArr)
-            if usuarioArchivoArr[0] == usuario:
-                print("1")
-                if usuarioArchivoArr[1] == str(key):
-                    print("1")
-                    if usuarioArchivoArr[2] == ruta:
-                        print("1")
+    rutaUsuarios = "/Users/Gonzalo/Desktop/FACULTAD/2019 s1/Seguridad/ProyectoSeguridad/Proyecto/usuarioarchivo.txt"
+    try:
+        with open(rutaUsuarios, 'r') as file:
+            lines = file.readlines()
+        file.close()
+        with open(rutaUsuarios, 'w') as fileOut:
+            for usuarioArchivo in lines:
+                if len(usuarioArchivo.split(",")) >=3:
+                    usuarioArchivoArr = obtenerDatos(usuarioArchivo)
+                    if (usuarioArchivoArr[0] == usuario) and (usuarioArchivoArr[1] == str(key)) and (usuarioArchivoArr[2].rstrip("\n") == ruta):
                         encriptador = Encryptor(key)
                         encriptador.decrypt_file(ruta)
-                        result = True
-    return result
-                
-        
-        
-                                
+                        res = True
+                    else:
+                        fileOut.write(usuarioArchivo)
+        fileOut.close()
+    except:
+        return False
+    return res
 
-    
-        
-        
+def obtenerDatos(linea):
+    datos = linea.split(",")
+    usuario = datos[0]
+    key = ""
+    ruta = datos[len(datos) -1]
+    if len(datos) > 3:
+        datosKey = (datos[1:len(datos)-1])
+        key = datosKey[0]
+        for i in range(1,len(datosKey)):
+            key += "," + datosKey[i]
+    elif len(datos) <= 3:
+        key = datos[1]
+    return [usuario,key,ruta]
 
 
